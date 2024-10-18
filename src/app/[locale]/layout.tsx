b55ae611010c09a2f1bde83ad11ce418edcfcf6d
kero <kerlossony@yahoo.com>
2024-10-18 14:48:44 +0300

    new production

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,5 +1,6 @@
 import { NextIntlClientProvider } from 'next-intl';
 import { getMessages } from 'next-intl/server';
+import { Head } from 'next/document';
 
 export default async function LocaleLayout({
     children,
@@ -12,6 +13,8 @@ export default async function LocaleLayout({
 
     return (
         <html lang={locale}>
+            <Head>
+                <meta name="google-site-verification" content="ymYfjvSdKiU-l4BrhY98KgFhOGRcRpehbUUsIKvpaW8" />            </Head>
             <body>
                 <NextIntlClientProvider messages={messages}>
                     {children}
