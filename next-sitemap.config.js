df572751144ba26f594556c6551b32f9df67dace
Kerlos Magdy <kerlosssony@gmail.com>
2024-12-04 17:14:38 +0200

    newsite Map

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,9 +1,14 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://caps-code.vercel.app', 
-    generateRobotsTxt: true,
-    sitemapSize: 5000,
-    changefreq: 'daily',
-    priority: 0.7,
-  };
-  
+  siteUrl: 'https://capscode.vercel.app',
+  generateRobotsTxt: true,
+  sitemapSize: 5000, 
+  changefreq: 'daily', 
+  priority: 0.7,
+  additionalPaths: async (config) => [
+    { loc: '/services', changefreq: 'weekly', priority: 0.8 },
+    { loc: '/about-us', changefreq: 'weekly', priority: 0.8 },
+    { loc: '/projects', changefreq: 'weekly', priority: 0.8 },
+    { loc: '/contact-us', changefreq: 'weekly', priority: 0.8 },
+  ],
+};
--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -14,6 +14,12 @@ export default async function LocaleLayout({
         <html lang={locale}>
             <head>
                 <meta name="google-site-verification" content="ymYfjvSdKiU-l4BrhY98KgFhOGRcRpehbUUsIKvpaW8" />
+                <link rel="canonical" href="https://caps-code.vercel.app/en/services" />
+                <link rel="canonical" href="https://caps-code.vercel.app/ar/services" />
+                <link rel="canonical" href="https://caps-code.vercel.app/en/projects" />
+                <link rel="canonical" href="https://caps-code.vercel.app/ar/projects" />
+                <link rel="canonical" href="https://caps-code.vercel.app/en/about-us" />
+                <link rel="canonical" href="https://caps-code.vercel.app/ar/about-us" />
             </head>
             <body>
                 <NextIntlClientProvider messages={messages}>
